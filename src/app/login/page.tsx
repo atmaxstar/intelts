92aabaf8d09d5c28ec8b85dddc15d0d2870374be
Atsutoshi Honda <haniyasusin@gmail.com>
2024-06-05 13:14:47 +0900

    ログインフォームを作成

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import Login from "./Login"
 
 const page = () => {
   return (
-    <div className="w-full h-5/6 flex justify-center items-center">
+    <div className="w-full bg-gray-200 h-full flex justify-center items-center">
       <Login/>
     </div>
   )
