6e5c32cef49562680b6e817d4586919bc267bad3
Atsutoshi Honda <haniyasusin@gmail.com>
2024-06-07 14:27:29 +0900

    音声認識未対応の画面対応を追加

--- src/app/ielts/StartPage.tsx
+++ src/app/ielts/StartPage.tsx
@@ -1,18 +1,31 @@
 import Link from 'next/link';
 import React from 'react'
+import { useSpeechRecognition } from 'react-speech-recognition';
 
 const StartPage = () => {
+
+  const {
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+
   return (
     <div className="flex flex-col items-center justify-center h-1/2 text-xl">
-      <div className='animate-fade-in-bottom mb-5'>
-      It starts with Part 1 and ends with Part3.
-      </div>
-      <div className='animate-fade-in-bottom'>
-      Are you Okay?
-      </div>
-      <Link className="px-16 py-1 mt-10 mb-5 text-red-500 border border-red-500 font-semibold rounded hover:bg-red-100 animate-fade-in" href={`/ielts/examination`}>
-        Start
-      </Link>
+      {browserSupportsSpeechRecognition ? 
+        <>
+          <div className='animate-fade-in-bottom mb-5'>
+          It starts with Part 1 and ends with Part3.
+          </div>
+          <div className='animate-fade-in-bottom'>
+          Are you Okay?
+          </div>
+          <Link className="px-16 py-1 mt-10 mb-5 text-red-500 border border-red-500 font-semibold rounded hover:bg-red-100 animate-fade-in" href={`/ielts/examination`}>
+            Start
+          </Link>
+        </>
+        :
+        <span className='mt-10 mb-10 text-red-500 text-xl'>Sorry, this browser does not support speech recognition.</span>
+      }
         
       <Link className="px-3 py-1 text-blue-500 border border-blue-500 font-semibold rounded hover:bg-blue-100 animate-fade-in" href={`/`}>
           Back
