2cb00a0c54afdc3b0a198d09e502709ac1eedac0
Atsutoshi-Honda-1161500 <Atsutoshi.Honda@lincolnuni.ac.nz>
2024-08-14 12:26:12 +1200

    link title to homepage

--- src/Layout/HeaderMyPage.tsx
+++ src/Layout/HeaderMyPage.tsx
@@ -1,15 +1,17 @@
 import { robotoslab } from '@/utils/font'
 import Logout from './Logout'
+import Link from 'next/link'
 
 const HeaderMyPage = () => {
   return (
     <nav className="sticky top-0 z-30 grid grid-cols-[1fr_max-content_1fr] items-center border-b bg-white p-5 shadow-sm">
       <div className="col-start-2">
-        <h1
+        <Link
+          href="/"
           className={`text-4xl font-bold text-red-800 ${robotoslab.className}`}
         >
           Intelts
-        </h1>
+        </Link>
       </div>
       <div className="justify-self-end">
         <Logout />
